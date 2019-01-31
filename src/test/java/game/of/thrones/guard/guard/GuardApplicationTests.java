f7a7e92d5d630be25add86f428ab75010e2909c8
Gehazzz <gehazzz@gmail.com>
2019-01-31 23:32:00 +0200

    Initial commit

---
+++ src/test/java/game/of/thrones/guard/guard/GuardApplicationTests.java
@@ -0,0 +1,17 @@
+package game.of.thrones.guard.guard;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GuardApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
